19fa0600b1b39a4344a39aa04f498b47d8eda446
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-01 16:22:12 +0430

    Update spam2.lua

--- plugins/spam2.lua
+++ plugins/spam2.lua
@@ -1,12 +1,19 @@
-do
-  local function run(msg, matches)
-    local text = matches[1]
-      return text
-      end
-        end
-    return {
-    patterns = {
-    '(.*)',
-    },
-    run = run,
-    }
+local function run(msg, matches)
+  local text = matches[1]
+  local b = 1
+
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  return text
+end
+
+return {
+  description = "Simplest plugin ever!",
+  usage = "!echo [whatever]: echoes the msg",
+  patterns = {
+    "^(.+)$"
+  }, 
+  run = run 
+}
